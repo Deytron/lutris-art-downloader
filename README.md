c6e81a52180bc7be702fcba5c3938458b06c9727
Deytron <hyperpikaevo@gmail.com>
2022-08-01 13:15:39 +0200

    reanme to main.py and added docs

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
 # Lutris Cover Art Downloader
 
+This is an **EXTREMELY DIRTY** script to download cover art for Lutris games. It first started as a personal project, but I'm putting it here in case anyone else wants to use it.
 
-# lutris-art-downloader
+It comes from a long time bug in Lutris. The source used to get cover arts is... unreliable. It's not a big deal, but it's annoying. So I wrote this script to download the cover arts from SteamGridDB.
+
+## Usage
+
+1. Clone the repository
+
+2. Install the dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+3. Run the script
+
+```bash
+python3 main.py
+```
+
+> You need a SteamGridDB API key. You can get one [here](https://www.steamgriddb.com/profile/settings/api).
+
+## Screenshots
+
+Your library will go from this:
+
+![No covers](https://i.imgur.com/GcyWlHA.png)
+
+To this:
+
+![Covers downloaded](https://i.imgur.com/SWYWqoy.png)
+
+In a matter of seconds.
+
+## How it works and warnings
+
+When I said the script dirty, it **IS** dirty. Obvisouly this won't get you any malware, but since I'm not that much into Python, I did what I could.
+What the script does is that it fetches the list of games from Lutris at `/home/$USER/.cache/lutris`, then it fetches the first cover art from SteamGridDB. It then saves the cover art in the Lutris cache folder.
+
+## Planned features
+
+- ❕ Argument support (--banner, --cover, --help)
+- ❕ Add a simple GUI with console output
+- ❕ Add a way to select the cover art you want via the GUI
+- Split in multiple files
+- Better code
+- Encode API Key in file
+- A better README?
+
+## Credits
+
+- Big thanks to the Lutris team!
+- Big thanks to SteamGridDB for their API and their resources!
+- Obvious thanks to StackOverflow!
---
+++ main.py
@@ -0,0 +1,130 @@
+# Import necessary modules
+import requests, sqlite3, os, inquirer, base64
+
+# Vars
+user = ''
+dbpath = ''
+dim = ''
+auth = ''
+covpath = ''
+
+# Main function
+def main():
+    global user, dbpath, dim, auth
+    print("Welcome " + user + " to Lutris Cover Art Downloader!\n")
+    user = GetUser()
+    dbpath = '/home/' + user + '/.local/share/lutris/pga.db'
+    dim = GetCoverType()
+    auth = GetAPIKey()
+    print("Getting API Key...\n")
+    if auth == '':
+        SetAPIKey()
+    co = DBConnect()
+    GetGamesList(co)
+
+
+####### FUNCTIONS
+
+
+def GetUser():
+    try:
+        return os.getlogin()
+    except:
+        print("Could not get session username")
+        exit(1)
+
+def GetCoverType():
+    global covpath
+    questions = [
+    inquirer.List('type',
+                    message="Would you like to download Steam banners or Steam vertical covers?",
+                    choices=['Banner (460x215)', 'Vertical (600x900)'],
+                ),
+    ]
+    ans = inquirer.prompt(questions)["type"]
+    print('Cover type set to ' + ans + '\n')
+    if ans == 'Banner (460x215)':
+        covpath = '/home/' + user + '/.cache/lutris/banners/'
+        dim = '460x215'
+    else:
+        covpath = '/home/' + user + '/.cache/lutris/coverart/'
+        dim = '600x900'
+    return dim
+
+def SaveAPIKey(key):
+    with open('./apikey.txt', 'w') as f:
+        f.write(key)
+
+def GetAPIKey():
+    if os.path.isfile('./apikey.txt'):
+        with open('./apikey.txt', 'r') as f:
+            key = f.read()
+            auth = {'Authorization': 'Bearer ' + key}
+            return auth
+    else:
+        return ''
+
+def SetAPIKey():
+    print("Could not find API key")
+    print('You need a SteamGriDB API key to use this script.')
+    print('You can get one by using your Steam account and heading here: https://www.steamgriddb.com/profile/preferences/api\n')
+    api = input("Enter your SteamGridDB API key: ")
+    auth = {'Authorization': 'Bearer ' + api}
+    TestAPI(auth, api)
+
+def TestAPI(key, api):
+    r = requests.get('https://www.steamgriddb.com/api/v2/grids/game/1?dimensions=600x900', headers=key)
+    if r.status_code == 200:
+        print("API key is valid, saving...")
+        SaveAPIKey(api)
+    else:
+        print("API key is invalid")
+        exit(1)
+
+def DBConnect():
+    try:
+        conn = sqlite3.connect(dbpath)
+    except:
+        print("Could not find Lutris database 'pga.db'. You can manually edit script's path if necessary")
+        exit(1)
+    return conn
+
+# Search for a game by name via Lutris database, then get the grid data
+def SearchGame(game):
+    res = requests.get('https://www.steamgriddb.com/api/v2/search/autocomplete/' + game, headers=auth).json()
+    if len(res) == 0:
+        print("Could not find a cover for game " + game)
+    else:
+        print("Found game " + game.replace('-', ' ').title())
+        id = res["data"][0]["id"]
+        return id
+
+# Download cover by searching for the game via its name, then via its SteamGriDB's ID
+def DownloadCover(name):
+    gameid = SearchGame(name)
+    print("Downloading cover for " + name.replace('-', ' ').title())
+    grids = requests.get('https://www.steamgriddb.com/api/v2/grids/game/' + str(gameid) + '?dimensions=' + dim, headers=auth).json()
+    try:
+        url = grids["data"][0]["url"]
+    except:
+        print("Could not find a cover for game " + name)
+        return
+    r = requests.get(url)
+    with open(covpath + name + '.jpg', 'wb') as f:
+        f.write(r.content)
+
+# Get all games and for each game, check if it already has a cover
+def GetGamesList(co):
+    c = co.execute('SELECT slug FROM games')
+    games = c.fetchall()
+    for entry in games:
+        title = entry[0]
+        if not os.path.isfile(covpath + title + '.jpg'):
+            # If not, download it
+            DownloadCover(title)
+        else:
+            print("Cover for " + title.replace('-', ' ').title() + " already exists")
+    print('All done ! Restart Lutris for the changes to take effect')
+
+if __name__ == '__main__':
+    main()
